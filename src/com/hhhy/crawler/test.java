9b74460510531d15d757600d8d98d4732879db5a
Ghost <shanjie@neotel.com.cn>
2014-06-03 16:53:40 +0800

    GhostSpider

---
+++ src/com/hhhy/crawler/test.java
@@ -0,0 +1,11 @@
+package com.hhhy.crawler;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ghost
+ * Date: 14-5-30
+ * Time: 上午9:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+}
