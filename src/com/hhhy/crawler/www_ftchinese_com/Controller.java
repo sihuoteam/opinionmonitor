2f12a86fd058cdd3f8be8e7cce0d016d174a2394
Ghost <shanjie@neotel.com.cn>
2014-06-05 11:03:47 +0800

    spiderUpdate

---
+++ src/com/hhhy/crawler/www_ftchinese_com/Controller.java
@@ -0,0 +1,11 @@
+package com.hhhy.crawler.www_ftchinese_com;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ghost
+ * Date: 14-6-4
+ * Time: 下午6:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class Controller {
+}
