1d553d9da46e1a5246e67d6edb61d2d325e27ba6
Ghost <shanjie@neotel.com.cn>
2014-06-09 11:29:02 +0800

    GhostSpider

---
+++ src/com/hhhy/crawler/www_eeo_com_cn/Controller.java
@@ -0,0 +1,11 @@
+package com.hhhy.crawler.www_eeo_com_cn;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ghost
+ * Date: 14-6-9
+ * Time: 上午11:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class Controller {
+}
