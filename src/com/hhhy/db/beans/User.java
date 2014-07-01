83ad212bd6baccd034ebb8fa0254881709fe16f8
GeekBUPT <forrest0579@gmail.com>
2014-07-01 09:47:17 +0800

    login

---
+++ src/com/hhhy/db/beans/User.java
@@ -0,0 +1,48 @@
+package com.hhhy.db.beans;
+
+public class User {
+    private String id;
+    private String nickname;
+    private String email;
+    private String password;
+
+    public User(String nickname, String email, String password) {
+        this.id = null;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
