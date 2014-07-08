5cff53b86b974b4297d19009bb313fecff6a6443
Ghost <shanjie@neotel.com.cn>
2014-07-08 17:31:23 +0800

    Spider

--- src/com/hhhy/crawler/bbs_hexun_com/Controller.java
+++ src/com/hhhy/crawler/bbs_hexun_com/Controller.java
@@ -1,5 +1,17 @@
 package com.hhhy.crawler.bbs_hexun_com;
 
+import com.hhhy.crawler.util.ContentFilter;
+import com.hhhy.crawler.util.FormatTime;
+import com.hhhy.crawler.util.GetHTML;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Ghost
@@ -8,4 +20,87 @@ package com.hhhy.crawler.bbs_hexun_com;
  * To change this template use File | Settings | File Templates.
  */
 public class Controller {
+    private final String BASE_URL = "http://bbs.hexun.com/search/?q=%B9%C9%C6%B1%BC%DB%B8%F1&type=1&Submit=";
+    private final String keyWordsLocation = "./keyWords.txt";
+    private static ArrayList<String> spyHistory = new ArrayList<String>();//the history record got earlier in today.
+
+    private ArrayList<String> keyWordsList = new ArrayList<String>();
+    Controller(){
+        File keyFile = new File(keyWordsLocation);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(keyFile));
+            String line = "";
+            while((line = br.readLine())!=null){
+                keyWordsList.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for(String keyWord:keyWordsList){
+            //Todo
+            parseBoard(keyWord,BASE_URL);
+        }
+    }
+
+    public static void parseBoard(String keyWord,String BASE_URL){
+        String transKey = "";
+        try {
+            transKey = URLEncoder.encode(keyWord, "gb2312");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String html = GetHTML.getHtml("http://bbs.hexun.com/search/?q="+transKey+"&type=1&Submit=", "gb2312");
+
+        html = html.replaceAll("&nbsp;","");
+        Document document = Jsoup.parse(html);
+        /*
+        搜索关键词是否存在
+         */
+        Elements flag = document.select("tr.bg");
+        if(flag.size()==0){
+            //Todo ??
+            System.out.println("nothing to found.....");
+        }
+        else{
+            Elements tableEles =  document.select("tr.bg");
+            ArrayList<Element> tableList = new ArrayList<Element>();
+            for(Element ele:tableEles){
+                tableList.add(ele);
+            }
+            parsePages(tableList);
+        }
+    }
+    public static void parsePages(ArrayList<Element> tableList){
+        for(Element ele:tableList){
+            String title = ele.select("td.f14").select("a").text();
+            String time = "20"+ele.select("td").last().select("p").text()+":00";
+            if(FormatTime.isAfterToday(time)){
+                if(!ContentFilter.redundant(spyHistory, title)){
+
+                    String summary = "";
+                    String url = ele.select("td.f14").select("a").attr("href");
+                    System.out.println("title:"+title);
+                    System.out.println("url:"+url);
+                    System.out.println("time:"+time);
+                    System.out.println("summary:"+summary);
+                    System.out.println("website:"+"和讯论坛");
+                    System.out.println("----------------");
+                    spyHistory.add(title);
+                    //调接口~~~~~
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        parseBoard("股票价格","");
+       /* String html = GetHTML.getHtml("http://search.cs.com.cn/newsSimpleSearch.do?searchword=%E8%B4%B7%E6%AC%BE&time=2&contentType=Content&pn=1","UTF-8");
+        Document document = Jsoup.parse(html);
+        Elements tables = document.select("div:has(div.hei12)");
+        for(Element ele:tables){
+            System.out.println(ele);
+        }*/
+        // System.out.println(java.net.URLEncoder.encode("投资基金", "UTF-8"));
+    }
 }
--- src/com/hhhy/crawler/util/FormatTime.java
+++ src/com/hhhy/crawler/util/FormatTime.java
@@ -1,5 +1,6 @@
 package com.hhhy.crawler.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -12,6 +13,16 @@ import java.util.Date;
  * To change this template use File | Settings | File Templates.
  */
 public class FormatTime {
+    public static Date getDate(String time){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+           Date date = simpleDateFormat.parse(time);
+            return date;
+        } catch (ParseException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
+    }
     public static String getCurrentFormatTime(){
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return simpleDateFormat.format(new Date());
@@ -28,6 +39,18 @@ public class FormatTime {
         calendar.set(Calendar.DATE,day-xDay);
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(calendar.getTime());
     }
+
+    public static boolean isAfterToday(String time){
+        String today = getCurrentFormatTime().split(" ")[0]+" 00:00:00";
+        Date Today = getDate(today);
+        Date Time = getDate(time);
+
+        if(Time.before(Today))
+            return false;
+        else
+            return true;
+    }
+
     public static void main(String[] args){
         System.out.println(getFormatTimeAfterXDays(1));
     }
