d6e2da678a79cb5d630a491e1713b9f6e3b8fcd2
Ghost <shanjie@neotel.com.cn>
2014-07-08 17:30:06 +0800

    Spider

---
+++ src/com/hhhy/crawler/bbs_hexun_com/Controller.java
@@ -0,0 +1,11 @@
+package com.hhhy.crawler.bbs_hexun_com;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ghost
+ * Date: 14-7-8
+ * Time: 下午3:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class Controller {
+}
