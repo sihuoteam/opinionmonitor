b67f8887594e2dfc3e2eaf13777eeb48b69a2ba2
chenlingpeng <forrest0579@gmail.com>
2014-08-10 17:47:15 +0800

    rm all jar

--- src/com/hhhy/core/lucene/FSIndexBuilder.java
+++ src/com/hhhy/core/lucene/FSIndexBuilder.java
@@ -49,7 +49,6 @@ public class FSIndexBuilder implements IndexBuilder {
         this.indexDir = indexDir;
         this.lastMerge = -1l;
         documentsCache = new ArrayList<Document>(CACHE_SIZE);
-        this.indexDir = indexDir;
         try {
             this.directory = new SimpleFSDirectory(new File(indexDir));
             analyzer = new AnsjAnalysis(StopWordsUtils.getStopWords(), false);
