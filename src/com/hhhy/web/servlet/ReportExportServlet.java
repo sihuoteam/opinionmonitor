7b37aeec8ce90fc8558b14cf542978696dd963f0
chenlingpeng <forrest0579@gmail.com>
2014-08-12 00:12:25 +0800

    data export servlet init

---
+++ src/com/hhhy/web/servlet/ReportExportServlet.java
@@ -0,0 +1,46 @@
+package com.hhhy.web.servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import com.hhhy.db.DBUtils;
+import com.hhhy.db.beans.KeyWord;
+
+/**
+ * report export
+ * @author chenlingpeng
+ * 
+ */
+public class ReportExportServlet extends HttpServlet {
+    private static final Logger logger = Logger.getLogger(ReportExportServlet.class);
+
+    private static final long serialVersionUID = 1L;
+
+    /* 
+     * 对应关键词的增删操作
+     */
+    @Override
+    protected void doGet(final HttpServletRequest req,
+            final HttpServletResponse resp) throws ServletException,
+            IOException {
+        // TODO: 
+        
+    }
+
+    @Override
+    protected void doPost(final HttpServletRequest req,
+            final HttpServletResponse resp) throws ServletException,
+            IOException {
+        doGet(req, resp);
+    }
+
+}
