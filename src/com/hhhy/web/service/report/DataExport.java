8a029c622d0ce5f0ae5f177b6cd7bc7acd6828eb
chenlingpeng <forrest0579@gmail.com>
2014-08-12 17:54:50 +0800

    data export edit

---
+++ src/com/hhhy/web/service/report/DataExport.java
@@ -0,0 +1,5 @@
+package com.hhhy.web.service.report;
+
+public class DataExport {
+
+}
--- src/com/hhhy/web/servlet/ReportExportServlet.java
+++ src/com/hhhy/web/servlet/ReportExportServlet.java
@@ -2,6 +2,7 @@ package com.hhhy.web.servlet;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -12,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import com.hhhy.common.utils.DateFormatUtils;
+import com.hhhy.common.utils.JsonUtils;
 import com.hhhy.db.DBUtils;
 import com.hhhy.db.beans.KeyWord;
 
@@ -33,7 +36,33 @@ public class ReportExportServlet extends HttpServlet {
             final HttpServletResponse resp) throws ServletException,
             IOException {
         // TODO: 
+        String start_date = req.getParameter("start_date");
+        String end_date = req.getParameter("end_date");
+        String[] merge = req.getParameterValues("merge");
+        String[] sources = req.getParameterValues("source[]");
+        String[] emotions = req.getParameterValues("sentiment[]");
+        String[] topic_ids = req.getParameterValues("topic_id[]");
+        String[] fields = req.getParameterValues("field[]");
+        String limit = req.getParameter("limit");
+        String source = JsonUtils.toJson(sources);
+        String emotion = JsonUtils.toJson(emotions);
+        String topic_id = JsonUtils.toJson(topic_ids);
+        String field = JsonUtils.toJson(fields);
+        logger.info(source);
+        logger.info(emotion);
+        logger.info(topic_id);
+        logger.info(field);
+        logger.info(limit);
+        logger.info(JsonUtils.toJson(merge));
+        try {
+            logger.info(DateFormatUtils.formatTime(DateFormatUtils.getTime(start_date, "yyyy-MM-dd"),DateFormatUtils.yyyyMMddhhmmss));
+        } catch (ParseException e) {
+            logger.warn(e.getMessage());
+        }
         
+        
+        
+        resp.sendRedirect("loginWeb.jsp");
     }
 
     @Override
