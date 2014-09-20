4c57a890f709e6cfa849c0bb662176016ecec7d2
chenlingpeng <forrest0579@gmail.com>
2014-09-20 14:48:05 +0800

    账号登陆独立

--- src/com/hhhy/web/service/webservice/dfcf/DFCFDingUtil.java
+++ src/com/hhhy/web/service/webservice/dfcf/DFCFDingUtil.java
@@ -1,6 +1,7 @@
 package com.hhhy.web.service.webservice.dfcf;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,10 +18,22 @@ public class DFCFDingUtil {
     private static final String reg01 = ".*,(.*),(.*)\\.html";
     private static final Pattern pattern01 = Pattern.compile(reg01);
 
-    public static boolean dingtie(String url, String cont) throws IOException {
-        // System.out.println("ding url: "+url+" "+cont);
+    private static NormalHttpClient login(String username, String psw) throws IOException {
+        username = URLEncoder.encode(username, "utf-8");
+        psw = URLEncoder.encode(psw, "utf-8");
+        System.out.println(username);
+        System.out.println(psw);
         CHttpGet get = new CHttpGet(
-                "http://passport.eastmoney.com/guba/AjaxAction.ashx?cb=jQuery18309460038826800883_1411024516265&op=login&dlm=wuxiu9218%40sina.com&mm=920108&vcode=&_=1411024534921");
+                "http://passport.eastmoney.com/guba/AjaxAction.ashx?cb=jQuery18309460038826800883_1411024516265&op=login&dlm="+username+"&mm="+psw+"&vcode=&_=1411024534921");
+        get.addHeader("Referer", "http://guba.eastmoney.com/");
+        get.addHeader("Host", "passport.eastmoney.com");
+        NormalHttpClient client = new NormalHttpClient();
+        HttpResponse response = client.sendRequest(get.getHttpGet());
+        EntityUtils.consume(response.getEntity());
+        return client;
+    }
+
+    public static boolean dingtie(String url, String cont) throws IOException {
         Matcher match01 = pattern01.matcher(url);
         String code = null;
         String topic_id = null;
@@ -30,14 +43,7 @@ public class DFCFDingUtil {
         }
         if (code == null || topic_id == null)
             return false;
-        get.addHeader("Referer", "http://guba.eastmoney.com/");
-        get.addHeader("Host", "passport.eastmoney.com");
-        NormalHttpClient client = new NormalHttpClient();
-        HttpResponse response = client.sendRequest(get.getHttpGet());
-        // System.out.println("content: "+ResponseUtils.getResponseContent(response));
-        // System.out.println("status: "+ResponseUtils.getResponseStatus(response));
-        String content = null;
-        EntityUtils.consume(response.getEntity());
+        NormalHttpClient client = login("wuxiu9218@sina.com","920108");
 
         CHttpPost post = new CHttpPost("http://guba.eastmoney.com/action.aspx");
         post.addHeader("Host", "guba.eastmoney.com");
@@ -51,12 +57,11 @@ public class DFCFDingUtil {
         post.addFormParam("code", code);
         post.addFormParam("yzm", "");
         post.addFormParam("yzm_id", "");
-        response = client.sendRequest(post.getHttpPost());
-        content = InputStreamUtils.entity2String(response.getEntity());
+        HttpResponse response = client.sendRequest(post.getHttpPost());
+        String content = InputStreamUtils.entity2String(response.getEntity());
         int status = ResponseUtils.getResponseStatus(response);
         EntityUtils.consume(response.getEntity());
-        // System.out.println("content: "+content);
-        // System.out.println("status: "+ResponseUtils.getResponseStatus(response));
+        client.releaseConnection();
         if (status != 200 || !content.contains("true"))
             return false;
         return true;
@@ -64,17 +69,7 @@ public class DFCFDingUtil {
 
     public static boolean fatie(String number, String title, String content)
             throws IOException {
-        CHttpGet get = new CHttpGet(
-                "http://passport.eastmoney.com/guba/AjaxAction.ashx?cb=jQuery18309460038826800883_1411024516265&op=login&dlm=wuxiu9218%40sina.com&mm=920108&vcode=&_=1411024534921");
-        get.addHeader("Referer", "http://guba.eastmoney.com/");
-        get.addHeader("Host", "passport.eastmoney.com");
-        NormalHttpClient client = new NormalHttpClient();
-        HttpResponse response = client.sendRequest(get.getHttpGet());
-        // System.out.println(ResponseUtils.getResponseContent(response));
-        // System.out.println("content: "+ResponseUtils.getResponseContent(response));
-        // System.out.println("status: "+ResponseUtils.getResponseStatus(response));
-        EntityUtils.consume(response.getEntity());
-
+        NormalHttpClient client = login("wuxiu9218@sina.com","920108");
         CHttpPost post = new CHttpPost("http://guba.eastmoney.com/action.aspx");
         // NormalHttpClient client = new NormalHttpClient();
         String host = "guba.eastmoney.com";
@@ -96,13 +91,15 @@ public class DFCFDingUtil {
         post.addFormParam("yzm", "");
         post.addFormParam("quanxian", "0");
         try {
-            response = client.sendRequest(post.getHttpPost());
-            String retC = ResponseUtils.getResponseContent(response).toLowerCase();
+            HttpResponse response = client.sendRequest(post.getHttpPost());
+            String retC = ResponseUtils.getResponseContent(response)
+                    .toLowerCase();
             int retCode = ResponseUtils.getResponseStatus(response);
             EntityUtils.consume(response.getEntity());
+            client.releaseConnection();
             if (retCode == 200 && retC.contains("true")) {
                 return true;
-            }else{
+            } else {
                 return false;
             }
         } catch (IOException e) {
@@ -116,7 +113,7 @@ public class DFCFDingUtil {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        // dingtie("http://guba.eastmoney.com/news,cjpl,123781934.html","顶3");
+//         dingtie("http://guba.eastmoney.com/news,cjpl,123781934.html","顶3");
         System.out.println("=======================");
         System.out.println(fatie("002314", "标题要长啊啊啊啊啊啊啊啊啊啊啊啊啊", "大家好"));
     }
